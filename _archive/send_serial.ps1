83f43af1e5a1254b621b79df63aa277ad7caacc3
songhan55 <songhan@pusan.ac.kr>
2026-08-15 23:30:09 +0900

    chore: remove unused archive files and consolidate work log into kinetic_realtime_system

--- _archive/send_serial.ps1
+++
@@ -1,8 +0,0 @@
-$port = New-Object System.IO.Ports.SerialPort "COM8", 9600
-$port.Open()
-Start-Sleep -Milliseconds 1000
-$port.WriteLine("1")
-Start-Sleep -Milliseconds 4500
-$out = $port.ReadExisting()
-$port.Close()
-Write-Host $out
